50540581c78d722ea138012c10eab3e8400342f8
yuhaoliu298 <yuhaoliu298@gmail.com>
2020-04-30 20:45:18 -0700

    Add colab file

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import GRU, LSTM, Bidirectional
-from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import MaxPool2D, GlobalMaxPool1D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
@@ -15,25 +15,25 @@ import matplotlib.pyplot as plt
 
 
 # Parameters
-TRAIN_SIZES = list(range(2500, 25000, 2500))
-BATCH_SIZE = 32
-LEARNING_RATE = 5e-5
-PATIENCE = 2
-EPOCHS = 15
-DROPOUT = 0.1
-HIDDEN_DIM = 128
-VOCAB_SIZE = 10000
-EMBEDDING_DIM = 16
-MAX_LEN = 512
-TRUNC_TYPE = "post"
-PADDING = "pre"
-OOV_TOKEN = "<OOV>"
-CALLBACK = tf.keras.callbacks.EarlyStopping(
-    monitor="val_loss", patience=PATIENCE, restore_best_weights=True
-)
+# TRAIN_SIZES = list(range(2500, 25000, 2500))
+# BATCH_SIZE = 32
+# LEARNING_RATE = 5e-5
+# PATIENCE = 2
+# EPOCHS = 15
+# DROPOUT = 0.1
+# HIDDEN_DIM = 128
+# VOCAB_SIZE = 10000
+# EMBEDDING_DIM = 16
+# MAX_LEN = 512
+# TRUNC_TYPE = "post"
+# PADDING = "pre"
+# OOV_TOKEN = "<OOV>"
+# CALLBACK = tf.keras.callbacks.EarlyStopping(
+#     monitor="val_loss", patience=PATIENCE, restore_best_weights=True
+# )
 
 
-def plot(history, string, count):
+def plot(history, string):
     """
     Plot training acc/loss and validation acc/loss
     """
@@ -42,100 +42,114 @@ def plot(history, string, count):
     plt.xlabel("Epochs")
     plt.ylabel(string)
     plt.legend(["train_" + string, "valid_" + string])
-    plt.savefig(f"trial{count}_{string}.png")
+    plt.show()
 
 
-def build_lstm():
-    """
-    Build a lstm layer model
-    """
-    model_lstm = Sequential()
-    model_lstm.add(Embedding(VOCAB_SIZE, EMBEDDING_DIM, input_length=MAX_LEN))
-    model_lstm.add(Dropout(DROPOUT))
-    model_lstm.add(LSTM(HIDDEN_DIM))
-    model_lstm.add(Dropout(DROPOUT))
-    model_lstm.add(Dense(1, activation="sigmoid"))
-    # Compile the model
-    model_lstm.compile(
+class BiLSTM:
+    def __init__(
+        self,
+        vocab_size=20000,
+        embedding_dim=50,
+        max_seq_len=128,
+        learning_rate=5e-5,
+        dropout=0.1,
+        hidden_dim=64,
         loss="binary_crossentropy",
-        optimizer=Adam(learning_rate=LEARNING_RATE),
-        metrics=["accuracy"],
-    )
-    model_lstm.summary()
-    return model_lstm
+    ):
+        self.vocab_size = 20000
+        self.embedding_dim = embedding_dim
+        self.max_seq_len = max_seq_len
+        self.learning_rate = learning_rate
+        self.dropout = dropout
+        self.hidden_dim = hidden_dim
+        self.loss = loss
+        self.training_sizes = list(range(1000, 23500, 1000))
+
+    def build_model(self):
+        model = Sequential(
+            [
+                Embedding(
+                    self.vocab_size, self.embedding_dim, input_length=self.max_seq_len
+                ),
+                Bidirectional(
+                    LSTM(
+                        self.hidden_dim,
+                        return_sequences=True,
+                        dropout=self.dropout,
+                        recurrent_dropout=self.dropout,
+                    )
+                ),
+                GlobalMaxPool1D(),
+                Dense(64, activation="relu"),
+                Dropout(self.dropout),
+                Dense(1, activation="sigmoid"),
+            ]
+        )
+        model.compile(
+            loss=self.loss,
+            optimizer="adam",  # Adam(learning_rate=self.learning_rate),
+            metrics=["accuracy"],
+        )
+        model.summary()
+        return model
 
 
-def train(train_size, trial_count):
-    imdb = tfds.load("imdb_reviews", as_supervised=True, shuffle_files=True)
+class IMDB:
+    def __init__(self, train_size, valid_size, test_size, vocab_size=20000, max_len=512):
+        self.train_size = train_size
+        self.valid_size = valid_size
+        self.test_size = test_size
+        self.vocab_size = vocab_size
+        self.max_len = max_len
+        self.dataset = tfds.load("imdb_reviews", as_supervised=True, shuffle_files=True)
+        self.train_dataset = self.dataset["train"]
+        self.test_dataset = self.dataset["test"]
 
-    train_data, test_data = imdb["train"], imdb["test"]
+    def preprocess_imdb(self):
+        # Group train/test sentences/labels
+        train_text = []
+        train_labels = []
+        test_text = []
+        test_labels = []
+        for s, l in self.train_dataset:
+            train_text.append(str(s.numpy()))
+            train_labels.append(l.numpy())
+        for s, l in self.test_dataset:
+            test_text.append(str(s.numpy()))
+            test_labels.append(l.numpy())
 
-    training_sentences = []
-    training_labels = []
-    testing_sentences = []
-    testing_labels = []
+        train_reviews, valid_reviews = train_text[: self.train_size], train_text[22500:]
+        train_sentiments, valid_sentiments = (
+            np.array(train_labels[: self.train_size]),
+            np.array(train_labels[22500:]),
+        )
+        test_reviews = test_text[: self.test_size]
+        test_sentiments = np.array(test_labels[: self.test_size])
 
-    for sentence, label in train_data:
-        training_sentences.append(str(sentence.numpy()))
-        training_labels.append(label.numpy())
+        tokenizer = Tokenizer(num_words=self.vocab_size, oov_token="<OOV>")
+        tokenizer.fit_on_texts(train_reviews)
 
-    for sentence, label in test_data:
-        testing_sentences.append(str(sentence.numpy()))
-        testing_labels.append(label.numpy())
+        train_seq = tokenizer.texts_to_sequences(train_reviews)
+        train_padded = pad_sequences(
+            train_seq, maxlen=self.max_len, truncating="post", padding="pre"
+        )
+        valid_seq = tokenizer.texts_to_sequences(valid_reviews)
+        valid_padded = pad_sequences(
+            valid_seq, maxlen=self.max_len, truncating="post", padding="pre"
+        )
+        test_seq = tokenizer.texts_to_sequences(test_reviews)
+        test_padded = pad_sequences(
+            test_seq, maxlen=self.max_len, truncating="post", padding="pre"
+        )
 
-    train_s = training_sentences[:train_size]
-    train_l = np.array(training_labels[:train_size])
+        print("Training sentences count:", len(train_padded))
+        print("Training labels count:", len(train_sentiments))
+        print("Validation sentences count:", len(valid_padded))
+        print("Validation labels count:", len(valid_sentiments))
+        print("Test sentences count:", len(test_padded))
+        print("Testing labels count:", len(test_sentiments))
 
-    valid_s = training_sentences[22500:]
-    valid_l = np.array(training_labels[22500:])
-
-    test_s = testing_sentences[:12500]
-    test_l = np.array(testing_labels[:12500])
-
-    print("Training sentences count:", len(train_s))
-    print("Training labels count:", len(train_l))
-    print("Validation sentences count:", len(valid_s))
-    print("Validation labels count:", len(valid_l))
-    print("Test sentences count:", len(test_s))
-    print("Testing labels count:", len(test_l))
-
-    tokenizer = Tokenizer(num_words=VOCAB_SIZE, oov_token=OOV_TOKEN)
-    tokenizer.fit_on_texts(train_s)
-
-    train_sequences = tokenizer.texts_to_sequences(train_s)
-    train_padded = pad_sequences(
-        train_sequences, maxlen=MAX_LEN, truncating=TRUNC_TYPE, padding=PADDING
-    )
-    valid_sequences = tokenizer.texts_to_sequences(valid_s)
-    valid_padded = pad_sequences(
-        valid_sequences, maxlen=MAX_LEN, truncating=TRUNC_TYPE, padding=PADDING
-    )
-    testing_sequences = tokenizer.texts_to_sequences(test_s)
-    testing_padded = pad_sequences(
-        testing_sequences, maxlen=MAX_LEN, truncating=TRUNC_TYPE, padding=PADDING
-    )
-
-    model_lstm = build_lstm()
-
-    history = model_lstm.fit(
-        train_padded,
-        train_l,
-        validation_data=(valid_padded, valid_l),
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-        epochs=EPOCHS,
-        callbacks=[CALLBACK],
-    )
-
-    # loss, acc = model_lstm.evaluate(testing_padded, test_l, batch_size=32)
-    plot(history, "accuracy", trial_count)
-    plot(history, "loss", trial_count)
-
-
-if __name__ == "__main__":
-    trial_counter = 0
-
-    for size in TRAIN_SIZES:
-        trial_counter += 1
-        print("=" * 10 + f" Trial {trial_counter}: {size} training examples " + "=" * 10)
-        train(size, trial_counter)
+        return (
+            (train_padded, valid_padded, test_padded),
+            (train_sentiments, valid_sentiments, test_sentiments),
+        )
