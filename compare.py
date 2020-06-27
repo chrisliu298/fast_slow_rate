b459d5d115c4a4a430d62ac4121eebf14a3ba957
convexlifefunction <yuhaoliu298@gmail.com>
2020-06-27 09:25:14 -0700

    Fix early stopping issue

--- compare.py
+++ compare.py
@@ -5,7 +5,11 @@ from sklearn.metrics import accuracy_score
 train_true = pd.read_csv("true25000.tsv", delimiter="\t")
 train_oracle = pd.read_csv("oracle25000.tsv", delimiter="\t")
 
+# true_reviews = train_true["reviews"]
 true_labels = train_true["true_labels"]
+# oracle_reviews = train_oracle["reviews"]
 oracle_labels = train_oracle["oracle_labels"]
 
 print(accuracy_score(true_labels, oracle_labels))
+
+# print(list(true_reviews) == list(oracle_reviews))
